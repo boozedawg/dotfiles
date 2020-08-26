cf54bc4753d3315a1c712c9366e5900d791e5d44
boozedawg <53592377+boozedawg@users.noreply.github.com>
2020-08-26 12:57:02 -0500

    Add files via upload

---
+++ debianinstall.sh
@@ -0,0 +1,13 @@
+#github pull commnad for dotfiles
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install neofetch neovim gparted openvpn network-manager
+
+sudo apt install qemu qemu-kvm virt-manager libvirt-daemon
+
+sudo apt install lightdm mate-desktop-environment mate-core
+
+sudo apt install cifs-utils samba ntfs-3g 
